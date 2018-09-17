e1de9797b1909285b3eb9b25d567272a82d521c7
pythorxxx <43121272+pythorxxx@users.noreply.github.com>
2018-09-18 00:00:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,76 +57,6 @@ client.channels.find('id', '491346280753266689').setName("༺Welcome To EGYPT༻
 
 
 
-console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-console.log('         [Wait please .. ]       ')
-console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-client.on('ready', () => {
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-  console.log(`Logged in as [ ${client.user.tag}! ]`);
-  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-  console.log('[           BOT IS ONLINE         ]')
-  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-  console.log('[        info         ]')
-  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-});
- 
-client.on("message", message => {
- 
-  function discoRole() {
-    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    roles.forEach((role) => {
-      let theRole = message.guild.roles.find("name", role);
-      theRole.edit({color: random}).catch(e => {
-        return message.channel.send(":x: **Error:** The role you specified in the `config.json` is either not a role on this server, or his a role higher than the highest role that I have.");
-      });
-    });
-  }
- 
-  if(message.content.startsWith(prefix + "startdisco")) {
-    if(allowedUsers.includes(message.author.id)) {
-    setInterval(() => { discoRole(); }, config.ms);
-    message.channel.send("```css\nDiscoing...```");
-    message.channel.send("Please make sure you read the README, you could get IP banned from discord because of ratelimits.");
-  } else {
-    message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
-  }
-} else
- 
-if(message.content.startsWith(prefix + "stopdisco")) {
-  if(allowedUsers.includes(message.author.id)) {
-  message.channel.send("I've stopped discoing.");
-  setTimeout(() => { console.log(process.exit(0)); }, 300);
-} else {
-  message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
-  }
-}
- 
-});
-const config = require('./config.json');
-
- 
-const allowedUsers = config.allowedUsers;
-const roles = config.roleToDisco;
- 
-client.on("ready", () => {
-    client.user.setPresence({ game: { name: `RAINBOW (= FX` }, type: 0 });
-    console.log("RAINBOW (= FX");
-});
-
 
 
 
@@ -158,6 +88,10 @@ client.on("ready", () => {
     });
 });
  
+
+
+
+
 
 client.on('ready', function(){
     var ms = 10000 ;
